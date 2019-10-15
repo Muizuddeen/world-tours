33bda13152753927d150a22790e0b6e443290fac
Muizuddeen <tabishmuiz@gmail.com>
2019-10-16 01:13:49 +0530

    first commit

---
+++ src/components/tourData.js
@@ -0,0 +1,30 @@
+export const tourData = [
+  {
+    id:1,
+    city: "new york",
+    img: "./img/newyork.jpg",
+    name: "new york bridge tour",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+  },
+  {
+    id:2,
+    city: "paris",
+    img: "./img/paris.jpg",
+    name: "paris lghts tour",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+  },
+  {
+    id:3,
+    city: "london",
+    img: "./img/london.jpg",
+    name: "london royal palace tour",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+  },
+  {
+    id:4,
+    city: "tokyo",
+    img: "./img/tokyo.jpg",
+    name: "tokyo tour",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+  }
+];
